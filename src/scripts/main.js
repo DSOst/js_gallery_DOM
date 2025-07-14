68f12f4984d446b298924eeb0faee7f9c42f4eff
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-07-14 23:14:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let link = e.target;
+
+  if (e.target.tagName === 'IMG') {
+    link = e.target.closest('a');
+  } else if (e.target.tagName === 'A') {
+    link = e.target;
+  }
+
+  if (link) {
+    const newSrc = link.getAttribute('href');
+
+    largeImg.src = newSrc;
+  }
+});
